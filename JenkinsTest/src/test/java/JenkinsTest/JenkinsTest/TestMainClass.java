acb1ac9572e0bbd7c1b4771afdd0bcbadd599659
cbevil <craige.bevil@stfc.ac.uk>
2021-02-08 21:34:08 +0000

    Added extra tests

--- JenkinsTest/src/test/java/JenkinsTest/JenkinsTest/TestMainClass.java
+++ JenkinsTest/src/test/java/JenkinsTest/JenkinsTest/TestMainClass.java
@@ -1,6 +1,6 @@
 package JenkinsTest.JenkinsTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,18 +10,31 @@ import org.junit.jupiter.api.Test;
 
 class TestMainClass {
 
+	MainClass mainClass;  
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		mainClass = new MainClass(); 
 	}
 
 	@Test
 	void testName() throws Exception {
-		MainClass mainClass = new MainClass(); 
 		assertTrue(mainClass.isMainClass());
 	}
 
-	@RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}") 
+	@Test
 	@DisplayName("Test addition")
+	void testAdd() {
+		assertEquals(Integer.valueOf(mainClass.add(3,5)),Integer.valueOf(8)); 
+	}
+
+	@Test
+	@DisplayName("Test subtract")
+	void testSubtract() {
+		assertEquals(Integer.valueOf(mainClass.subtract(8,3)),Integer.valueOf(5)); 
+	}
+
+	@RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}") 
 	void testAdd(RepetitionInfo repInfo) throws Exception {
 		System.out.println(repInfo.getTotalRepetitions());
 		MainClass mainClass = new MainClass(); 
